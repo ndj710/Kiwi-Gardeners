a1ce7c105ecdc18d634b5e6b7ed2c1ea2b2adc1f
ndj710 <nathan.d.janssen@gmail.com>
2022-02-04 16:25:40 +1300

    Update EditJobData.js

--- Journal App/components/EditJobData.js
+++ Journal App/components/EditJobData.js
@@ -177,13 +177,11 @@ export default class EditData extends React.Component {
 								this.setState({status: selectedItem})
 							}}
 							buttonTextAfterSelection={(selectedItem, index) => {
-								// text represented after item is selected
-								// if data array is an array of objects then return selectedItem.property to render after item is selected
+
 								return selectedItem
 							}}
 							rowTextForSelection={(item, index) => {
-								// text represented for each item in dropdown
-								// if data array is an array of objects then return item.property to represent item in dropdown
+
 								return item
 							}}
 						/>
